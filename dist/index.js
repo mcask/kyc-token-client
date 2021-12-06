c5f3b3b4c815e31615940430edf8345a94e49108
Nim Siriwardana <nim@metacask.com>
2021-12-06 10:21:37 +0100

    exporting some classes

--- dist/index.js
+++ dist/index.js
@@ -12,4 +12,12 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./kyc-token-client"), exports);
 __exportStar(require("./gateway-token"), exports);
+var executor_base_1 = require("./executor-base");
+Object.defineProperty(exports, "CasperExecutor", { enumerable: true, get: function () { return executor_base_1.CasperExecutor; } });
+var node_resolver_1 = require("./node-resolver");
+Object.defineProperty(exports, "NodeResolver", { enumerable: true, get: function () { return node_resolver_1.NodeResolver; } });
+var casper_signer_executor_1 = require("./casper-signer-executor");
+Object.defineProperty(exports, "CasperSignerExecutor", { enumerable: true, get: function () { return casper_signer_executor_1.CasperSignerExecutor; } });
+var local_casper_executor_1 = require("./local-casper-executor");
+Object.defineProperty(exports, "LocalCasperExecutor", { enumerable: true, get: function () { return local_casper_executor_1.LocalCasperExecutor; } });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from "./kyc-token-client"
 export * from './gateway-token'
+export {CasperExecutor} from "./executor-base"
+export {NodeResolver} from "./node-resolver"
+export {CasperSignerExecutor} from "./casper-signer-executor"
+export {LocalCasperExecutor} from "./local-casper-executor"
